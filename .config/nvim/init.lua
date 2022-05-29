9ed93eab6addcd19e7fc857fc8f8012185411f76
Henry Kupty <hkupty@gmail.com>
2022-05-29 23:12:41 +0200

    Fix monitors for home setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,26 @@
 -- luacheck: globals vim
 require("deps")
 require("globals")
+require('impatient')
 require("plugins")
+
+-- Setting up individual plugins
+require("plugins.luasnip")
 require("plugins.cmp")
+require("plugins.nvim_comment")
 require("plugins.gitsigns")
 require("plugins.iron")
 require("plugins.lsp")
 require("plugins.lualine")
 require("plugins.orgmode")
 require("plugins.sidebar")
+require("plugins.neogen")
 require("plugins.telescope")
 require("plugins.treesitter")
+
 require("config")
 require("keybindings")
+
 
 vim.cmd [[source $HOME/.config/nvim/config.vim]]
 
